50614e4658e0ad383a3bfc3719adb5f1df6dc160
Saurav <holysoul55051052@gmail.com>
2025-10-19 12:49:00 +0545

    Initial Commit

---
+++ premake5.lua
@@ -0,0 +1,30 @@
+
+outputdir = "%{cfg.system}/%{cfg.buildcfg}"
+build_directory = "bin/builds/" ..outputdir.. "/%{prj.name}"
+intermediates_directory = "bin/intermediates/" ..outputdir.. "/%{prj.name}"
+
+project "Glad"
+	kind "StaticLib"
+	language "C"
+	systemversion "latest"
+	staticruntime "On" 
+
+	targetdir (build_directory)
+	objdir (intermediates_directory)
+
+	files
+	{
+		"include/glad/glad.h",
+		"include/KHR/khrplatform.h",
+		"src/glad.c"
+	}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+		optimize "off"
+
+	filter "configurations:Release"
+		runtime "Release"
+		symbols "off"
+		optimize "on"
